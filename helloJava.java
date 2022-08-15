08f594e1f761c827c097470b696a039e8d3a0ad7
Your Name <you@example.com>
2022-08-15 09:13:27 +0700

    first commit

---
+++ helloJava.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class helloJava {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("please enter a number 0 till 1000!");
+        System.out.println("we'll check and print every prime number 0 till 1000");
+        //gan mac dinh prime la false
+       boolean isPrime = false;
+    int a = scanner.nextInt();
+    //mac dinh so 1 ,2 la so nguyen to ko can check
+        System.out.print(1+" ");
+        System.out.print(2+" ");
+
+        if(a>2&&a<1000){
+                for (int i=3;i<=a;i++){
+                    //gan true neu i la so nguyen to
+                    isPrime =true;
+                    for (int j =2;j<i;j++){
+                        //check i neu ko la so nguyen to gan false
+                        if(i%j==0){
+                            isPrime =false;
+                        }
+                    }
+                    //in ra so nguyen to tu 0->a
+                    if (isPrime==true){
+                        System.out.print(i+" ");
+                    }
+                }
+        } else {
+            System.out.println("so nhap khong hop le");
+        }
+
+         }
+        }
